3defdd6f813ebf293ac3643df7ea51fac00df14d
Filipe Bastos <106710098+Slaverxen@users.noreply.github.com>
2022-08-12 22:21:22 -0300

    Estrutura Inicial

---
+++ src/GerenciamentoFuncionario.Comuns/Modelos/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciamentoFuncionario.Comuns.Modelos
+{
+    class Funcionario
+    {
+        public int Id { get; protected set; }
+
+        public string NomeCompleto { get; protected set; }
+
+        public  int CargoId { get; protected set; }
+
+        public bool EbebedorCafe { get; protected set; }
+
+        public string PrimeiroNome { get; protected set; }
+
+        public string UltimoNome { get; protected set; }
+
+        public DateTimeOffset DataEntrada { get; protected set; }
+
+
+
+
+
+    }
+}
